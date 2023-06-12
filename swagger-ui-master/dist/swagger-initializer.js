6c59529fd60c185e920e215e34dbc9a20cd0292e
PHILLIP\pov_ken <pov_ken@phillipbank.com.kh>
2023-06-12 12:03:36 +0700

    upate main file

--- swagger-ui-master/dist/swagger-initializer.js
+++ swagger-ui-master/dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "",
+    url: "https://raw.githubusercontent.com/povken/povken.github.io/main/main_page.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
